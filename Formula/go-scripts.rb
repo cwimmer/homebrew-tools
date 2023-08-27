0a9d7d26f3b0a2c3e1ba61444a9ea1bd0d56e9eb
Charles Wimmer <charles@amplitude.com>
2023-08-27 14:36:38 -0700

    releasing go-scripts v0.2.0

---
+++ Formula/go-scripts.rb
@@ -0,0 +1,31 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class GoScripts < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/cwimmer/go-scripts/archive/refs/tags/v0.2.0.tar.gz"
+  version "v0.2.0"
+  sha256 "e95f5f28316ff538ba1cba221cebd1438ea55bae10324cc08e031e4b3a2f2b66"
+  license ""
+
+  depends_on "go" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test go-scripts`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
