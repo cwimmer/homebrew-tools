c890df6899256d74ecac52c3d85fa68bfe45e4f3
Charles Wimmer <charles@amplitude.com>
2023-08-27 14:55:31 -0700

    releasing go-scripts v0.2.0

--- Formula/go-scripts.rb
+++ Formula/go-scripts.rb
@@ -4,7 +4,7 @@ class GoScripts < Formula
   url "https://github.com/cwimmer/go-scripts/archive/refs/tags/v0.2.0.tar.gz"
   version "0.2.0"
   sha256 "e95f5f28316ff538ba1cba221cebd1438ea55bae10324cc08e031e4b3a2f2b66"
-  license "Apache 2.0"
+  license "Apache-2.0"
 
   depends_on "go" => :build
 
